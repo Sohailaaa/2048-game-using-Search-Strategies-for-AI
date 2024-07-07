91f44748e96321ee23fc5cc1e1b42cf4c03bcd87
AseelAmrr <127628826+AseelAmrr@users.noreply.github.com>
2024-07-07 19:08:18 +0300

    boundaryCheck

--- AI_Game/src/generic_search.java
+++ AI_Game/src/generic_search.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+<<<<<<< Updated upstream
 import java.util.Random;
 
 public class generic_search {
@@ -82,6 +83,107 @@ public class generic_search {
 		return "A* Search 2 result";
 	}
 
+=======
+
+public class generic_search {
+	public static int row = 1;
+	public static int column = 1;
+	public static int[][] grid = new int[1][1];
+
+	public static boolean boundaryCheck(int[] coordinates) {
+		if (coordinates[0] < 0 || coordinates[0] > row - 1) // checks rows x
+			return true;
+		else if (coordinates[1] < 0 || coordinates[1] > column - 1) // checks columns y
+			return true;
+
+		return false;
+
+	}
+
+	public static String search(int[][] gridG, String strategy, boolean visualize) {
+		grid = gridG;
+		row = grid.length - 1;
+		column = grid[0].length - 1;
+		Map<Integer, int[]> coordinatesMap = reshape(grid);
+		printCoordinatesMap(coordinatesMap);
+		switch (strategy) {
+		case "BF":
+			return breadthFirstSearch();
+		case "DF":
+			return depthFirstSearch();
+		case "ID":
+			return iterativeDeepeningSearch();
+		case "GR1":
+			return greedySearch1();
+		case "GR2":
+			return greedySearch2();
+		case "AS1":
+			return aStarSearch1();
+		case "AS2":
+			return aStarSearch2();
+		default:
+			throw new IllegalArgumentException("Unknown strategy: " + strategy);
+		}
+	}
+
+	public static Map<Integer, int[]> reshape(int[][] grid) {
+		Map<Integer, int[]> coordinatesMap = new HashMap<>();
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				if (grid[i][j] != 0) {
+					coordinatesMap.put(grid[i][j], new int[] { i, j });
+				}
+			}
+		}
+
+		return coordinatesMap;
+	}
+
+	public static void printCoordinatesMap(Map<Integer, int[]> coordinatesMap) {
+		for (Map.Entry<Integer, int[]> entry : coordinatesMap.entrySet()) {
+			int key = entry.getKey();
+			int[] coordinates = entry.getValue();
+			System.out.println("Key: " + key + " -> Coordinates: [" + coordinates[0] + ", " + coordinates[1] + "]");
+		}
+	}
+
+	private static String breadthFirstSearch() {
+		// Implement the breadth-first search algorithm here
+		return "Breadth-First Search result";
+	}
+
+	private static String depthFirstSearch() {
+		// Implement the depth-first search algorithm here
+		return "Depth-First Search result";
+	}
+
+	private static String iterativeDeepeningSearch() {
+		// Implement the iterative deepening search algorithm here
+		return "Iterative Deepening Search result";
+	}
+
+	private static String greedySearch1() {
+		// Implement the first greedy search algorithm here
+		return "Greedy Search 1 result";
+	}
+
+	private static String greedySearch2() {
+		// Implement the second greedy search algorithm here
+		return "Greedy Search 2 result";
+	}
+
+	private static String aStarSearch1() {
+		// Implement the first A* search algorithm here
+		return "A* Search 1 result";
+	}
+
+	private static String aStarSearch2() {
+		// Implement the second A* search algorithm here
+		return "A* Search 2 result";
+	}
+
+>>>>>>> Stashed changes
 	public static void main(String[] args) {
 
 	}

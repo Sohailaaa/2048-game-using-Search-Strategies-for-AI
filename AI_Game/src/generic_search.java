33f55dc984e3f2b5c7c7aba974f76a126a85af58
Sohaila Hakeem <98600518+Sohailaaa@users.noreply.github.com>
2024-07-12 01:30:05 +0300

    Update generic_search.java

--- AI_Game/src/generic_search.java
+++ AI_Game/src/generic_search.java
@@ -10,33 +10,43 @@ import java.util.Random;
 import java.util.Set;
 
 public class generic_search {
-	public static int row = 1;
-	public static int column = 1;
-	public static int[][] grid = new int[1][1];
-	public static ArrayList<Integer> organisms_id = new ArrayList<>();
+	public static int row;
+	public static int column;
+	public static int[][] grid;
+	public static ArrayList<Organism> organismList;
 
 	static List<int[]> reconstructPath(Node node) {
 		List<int[]> path = new ArrayList<>();
 		while (node != null) {
-			path.add(node.action);
+			if (node.action != null) {
+				path.add(node.action);
+			}
 			node = node.parent;
 		}
 		Collections.reverse(path);
+		for (int[] p : path) {
+			System.out.print("recons" + p[0] + "." + p[1]);
+		}
 		return path;
 	}
 
 	static List<String> PathMapper(List<int[]> path) {
+//		System.out.println("path"+path.iter);
+		for (int[] p : path) {
+			System.out.print("recons" + p[0] + "." + p[1]);
+		}
 		String[] directionMapping = { "North", "South", "East", "West" };
 
 		List<String> mappedPath = new ArrayList<>();
-
+		if (path == null)
+			return null;
 		for (int[] point : path) {
 			int row = point[0];
 			int col = point[1];
 
 			String organism = "Organism_" + (row);
 			String direction = directionMapping[col - 1]; // Assuming col corresponds to direction index
-
+			String state = "" + organism + "_" + direction + ",";
 			mappedPath.add(organism);
 			mappedPath.add(direction);
 		}
@@ -45,40 +55,20 @@ public class generic_search {
 	}
 
 	public static boolean goalTest(Node n) {
-		if (n.organisms_id.size() == 1)
+		if (n.organismList.size() == 1)
 			return true;
 		return false;
 	}
+	
+	
 
-	public static boolean obstacleCheck(int[] organismCoordinates, Map<Integer, int[]> coordinatesMap) {
-		for (Map.Entry<Integer, int[]> entry : coordinatesMap.entrySet()) {
-			if (entry.getKey() == -1) { // Only consider obstacles
-				int[] obstacleCoordinates = entry.getValue();
-				if (obstacleCoordinates[0] == organismCoordinates[0]
-						&& obstacleCoordinates[1] == organismCoordinates[1]) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	public static boolean boundaryCheck(int[] coordinates) {
-		if (coordinates[0] < 0 || coordinates[0] > row - 1) // checks rows x
-			return true;
-		else if (coordinates[1] < 0 || coordinates[1] > column - 1) // checks columns y
-			return true;
-
-		return false;
-
-	}
 
 	public static List<String> search(int[][] gridG, String strategy, boolean visualize) {
-		grid = gridG;
-		row = grid.length;
-		column = grid[0].length;
-		Map<Integer, int[]> coordinatesMap = reshape(grid);
-		printCoordinatesMap(coordinatesMap);
+		row = gridG.length;
+		column = gridG[0].length;
+		organismList = new ArrayList<>();
+		Map<Integer, int[]> coordinatesMap = reshape(gridG);
+		// printCoordinatesMap(coordinatesMap);
 		switch (strategy) {
 		case "BF":
 			return PathMapper(reconstructPath(breadthFirstSearch(coordinatesMap)));
@@ -99,19 +89,27 @@ public class generic_search {
 		}
 	}
 
+	// Transforms grid from 2D array of integers, into a map of
+	// integers with their corresponding coordinates.
 	public static Map<Integer, int[]> reshape(int[][] grid) {
 		Map<Integer, int[]> coordinatesMap = new HashMap<>();
 
 		for (int i = 0; i < grid.length; i++) {
 			for (int j = 0; j < grid[i].length; j++) {
 				if (grid[i][j] != 0) {
+
 					coordinatesMap.put(grid[i][j], new int[] { i, j });
-					if (grid[i][j] != -1)
-						organisms_id.add(grid[i][j]);
+
 				}
+				if (grid[i][j] > 0) {
+					Organism org = new Organism(grid[i][j],1,i,j);
+					organismList.add(org);
+
+				}
+
 			}
 		}
-
+		System.out.println("hashmap" + coordinatesMap);
 		return coordinatesMap;
 	}
 
@@ -119,55 +117,82 @@ public class generic_search {
 		for (Map.Entry<Integer, int[]> entry : coordinatesMap.entrySet()) {
 			int key = entry.getKey();
 			int[] coordinates = entry.getValue();
-			System.out.println("Key: " + key + " -> Coordinates: [" + coordinates[0] + ", " + coordinates[1] + "]");
+			System.out.println(
+					"Hashmap: " + "Key: " + key + " -> Coordinates: [" + coordinates[0] + ", " + coordinates[1] + "]");
 		}
+	}
+
+	public static boolean checkSameState(Node a, Node b) {
+		if (a.action == null || b.action == null)
+			return false;
+		int org1 = b.action[0];
+
+		if ((a.grid.get(org1)[0] == b.grid.get(org1)[0]) && (a.grid.get(org1)[1] == a.grid.get(org1)[1])) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static ArrayList<Organism> organismListClone(ArrayList<Organism> original) {
+		ArrayList<Organism> clone = new ArrayList<>();
+
+		for (Organism o : original) {
+			clone.add(o.clone());
+		}
+
+		return clone;
 	}
 
 	public static List<Node> expand(Node node) {
 		List<Node> children = new ArrayList<>();
-		// Create a copy of organisms_id to iterate over
-		List<Integer> organismsCopy = new ArrayList<>(node.organisms_id);
+		ArrayList<Organism> organismsCopy = organismListClone(node.organismList);
 
 		for (int i = 0; i < organismsCopy.size(); i++) {
 			for (int j = 1; j <= 4; j++) {
-				System.out.print("hi" + node.grid);
-				System.out.print("organisms" + node.organisms_id);
-				System.out.print("i" + i);
-				int[] new_coordinate = node.move(node.grid.get(organismsCopy.get(i)), j);
-				if (!(boundaryCheck(new_coordinate) || obstacleCheck(new_coordinate, node.grid))) {
-					int[] action = new int[] { organismsCopy.get(i), j };
-					Node child = new Node(node.grid, node, action, node.organisms_id, node.cost);
-					child.updateGrid();
+				int[] action = new int[] { organismsCopy.get(i).getId(), j };
+				// Create a deep copy of the grid
+				Map<Integer, int[]> childGrid = new HashMap<>(node.grid);
+				ArrayList<Organism> childOrganismList = organismListClone(node.organismList);
+
+				Node child = new Node(childGrid, node, action, childOrganismList, node.cost);
+				// System.out.print("nodeGrid " + node.grid);
+				int id = organismsCopy.get(i).getId();
+				int[] coordinates = childGrid.get(id);
+				child.move(coordinates, action);
+				if (!child.isDead)
 					children.add(child);
-				}
 			}
 		}
 		return children;
 	}
 
 	private static Node breadthFirstSearch(Map<Integer, int[]> gridInitial) {
-		Node root = new Node(gridInitial, null, null, organisms_id, 0);
+		System.out.println(organismList + "orgList");
+
+		Node root = new Node(gridInitial, null, null, organismList, 0);
 		if (goalTest(root)) {
+			System.out.print("root");
 			return root;
 		}
 		Deque<Node> queue = new ArrayDeque<>();
-		Set<Map<Integer, int[]>> visited = new HashSet<>();
 		queue.add(root);
-		visited.add(deepCopyGrid(root.grid)); // Assuming a method to deep copy the grid map
 
 		while (!queue.isEmpty()) {
 			Node node = queue.poll();
-			if (goalTest(node)) {
-				return node;
-			}
+
 			for (Node child : expand(node)) {
-				Map<Integer, int[]> childGridCopy = deepCopyGrid(child.grid);
-				if (!visited.contains(childGridCopy)) {
-					queue.add(child);
-					visited.add(childGridCopy);
+				if (goalTest(child)) {
+					System.out.print(
+							child.organismList.get(0).getId() + "heeeeeeey" + child.organismList.get(0).size + "  " + child.grid + "  with cost: " + child.cost);
+
+					return child;
 				}
+				queue.add(child);
+
 			}
 		}
+		System.out.print("noooo");
 		return null;
 	}
 
